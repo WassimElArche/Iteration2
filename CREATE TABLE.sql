abdbe6b4b8f644250090471d34856511fab32d44
Mehdi <mehdisse20@gmail.com>
2024-11-04 15:49:21 +0100

    ajout de la creation des tables

---
+++ CREATE TABLE.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Ligue (
+    ID_Ligue INT,
+    nomLigue VARCHAR(100) NOT NULL,
+    ADD CONSTRAINT PK_Ligue PRIMARY KEY (ID_Ligue)
+)engine = innodb;
+
+CREATE TABLE Employe (
+    ID_Employe INT,
+    prenomEmploye VARCHAR(50) NOT NULL,
+    nomEmploye VARCHAR(50) NOT NULL,
+    mail VARCHAR(100) UNIQUE NOT NULL,
+    passwd VARCHAR(50) NOT NULL,
+    datearv DATE NOT NULL,
+    datedepart DATE,
+    Admin BOOLEAN NOT NULL,
+    ID_Ligue INT,
+    ADD CONSTRAINT PK_Employe PRIMARY KEY (ID_Employe)
+)engine = innodb;
+
+ALTER TABLE Employe
+ADD CONSTRAINT fk_Ligue_employe
+FOREIGN KEY (ID_Ligue) 
+REFERENCES Ligue(ID_Ligue);
